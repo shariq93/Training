7e547c0170890e39d99c20d30784e4a11de563fd
Shariq Khan <shariqsb93@gmail.com>
2023-06-19 10:52:21 +0500

    Search Text field

---
+++ src/components/UserField.js
@@ -0,0 +1,19 @@
+
+
+import { TextInput, } from 'react-native';
+import MyTextFiled from './MyTextFiled';
+
+
+const UserField = ({ onChange, placeHolder, style }) => {
+
+
+
+    return <MyTextFiled
+        style={{borderColor:'red',backgroundColor:'#ccc'}}
+    />
+
+}
+
+
+
+export default UserField
--- src/screens/UsersScreen.js
+++ src/screens/UsersScreen.js
@@ -4,12 +4,13 @@ import { Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-nati
 import UserListItem from '../components/UserListItem';
 import { TextInput } from 'react-native';
 import MyTextFiled from '../components/MyTextFiled';
+import UserField from '../components/UserField';
 
 
 
 export default function App() {
 
-  const userList = [
+  const [userList, setUserList] = useState([
     { name: 'John Doe', email: 'johndoe@example.com' },
     { name: 'Alice Smith', email: 'alicesmith@example.com' },
     { name: 'Robert Johnson', email: 'robertjohnson@example.com' },
@@ -20,32 +21,32 @@ export default function App() {
     { name: 'Jennifer Taylor', email: 'jennifertaylor@example.com' },
     { name: 'Christopher Thomas', email: 'christopherthomas@example.com' },
     { name: 'Jessica Martinez', email: 'jessicamartinez@example.com' }
-  ]
+  ])
+
+  const [userListCopy, setUserListCopy] = useState(userList)
+
 
   const [query, setQuery] = useState("")
   useEffect(() => {
-
-
-    const filter = (item) => {
-
-      if (item.name.includes(query)) { return true }
-      else return false
+    if (query != '') {
+      const filter = (item) => item.name.includes(query)
+      const fList = userList.filter(filter)
+      setUserList(fList)
+    }else{
+      setUserList(userListCopy)
     }
-    const fList = userList.filter(filter)
-    console.log(fList);
   }
 
     , [query])
 
   return (
     <View style={{
-      justifyContent: 'center',
       flex: 1,
       backgroundColor: '#ecf0f1',
       paddingTop: 45,
     }}>
       <View style={{ paddingHorizontal: 20 }}>
-
+<UserField/>
         <MyTextFiled
           onChange={(text) => {
             setQuery(text)
