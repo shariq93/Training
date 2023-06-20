4457dad7efeff35687055cf569ddfef953dd5e47
Shariq Khan <shariqsb93@gmail.com>
2023-06-20 10:48:44 +0500

    get Product with async await

--- src/screens/UsersScreen.js
+++ src/screens/UsersScreen.js
@@ -17,28 +17,35 @@ export default function App() {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(false)
   useEffect(() => {
-    setLoading(true)
-    fetch('https://dummyjson.com/products').then((response) => response.json())
-      .then((data) => {
-        setProducts(data.products)
-        setLoading(false)
-      })
+    getProducts()
   }, [])
-
+  const getProducts = async () => {
+   try {
+    setLoading(true)
+    const response = await fetch('https://dummyjson.com/products')
+    const data = await response.json()
+    setProducts(data.products)
+    setLoading(false)
+   } catch (error) {
+    alert(error)
+   }
+  
+  }
   return (
     <View style={{
       flex: 1,
       backgroundColor: '#ecf0f1',
-     
+
     }}>
-    {loading &&   <ActivityIndicator color={'#333'} />}
+      {loading && <ActivityIndicator color={'#333'} />}
       <FlatList
-      numColumns={2}
-      data={products}
-      renderItem={({item})=> {
-        console.log(item);
-      return <ProductItem data={item}/>} }
-      
+        numColumns={2}
+        data={products}
+        renderItem={({ item }) => {
+          console.log(item);
+          return <ProductItem data={item} />
+        }}
+
       />
     </View>
   );
